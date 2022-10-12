f98366f989d09ebc6011261f1ae0f15f9306462b
Yusuf Saraç <70776379+yusufsarac98@users.noreply.github.com>
2022-10-12 21:37:26 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
-const readingMode = document.getElementById('toggleEye');
 
 toggle.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
